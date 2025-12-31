e71d7dec6feb8b64de830d7d9fda49c50945a32a
Alberto M <traquinedes@yahoo.es>
2025-12-31 18:34:05 +0100

    feat[web-ui]: display whatsapp message in the web

--- src/webui/app.py
+++ src/webui/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, flash
-from webui.forms import WeeklyPlannerForm
+from webui.forms import WeeklyPlannerForm, STARTERS, MAIN_COURSES, DINNER
+from message_templates.whatsapp import build_whatsapp_meal_plan
 
 import os
 SECRET_KEY = os.urandom(32)
@@ -12,12 +13,49 @@ app.config['SECRET_KEY'] = SECRET_KEY
 def index():
     form = WeeklyPlannerForm()
     if form.validate_on_submit():
-        weekly_plan = {'sabado': {'lunch': {'first': form.saturday_lunch_starter.data, 'second': form.saturday_lunch_main.data}, 'dinner': form.saturday_dinner.data},
-                       'domingo': {'lunch': {'first': form.sunday_lunch_starter.data, 'second': form.sunday_lunch_main.data}, 'dinner': form.sunday_dinner.data},
-                       'lunes': {'lunch': {'first': form.monday_lunch_starter.data, 'second': form.monday_lunch_main.data}, 'dinner': form.monday_dinner.data},
-                       'martes': {'lunch': {'first': form.tuesday_lunch_starter.data, 'second': form.tuesday_lunch_main.data}, 'dinner': form.tuesday_dinner.data},
-                       'miercoles': {'lunch': {'first': form.wednesday_lunch_starter.data, 'second': form.wednesday_lunch_main.data}, 'dinner': form.wednesday_dinner.data},
-                       'jueves': {'lunch': {'first': form.thursday_lunch_starter.data, 'second': form.thursday_lunch_main.data}, 'dinner': form.thursday_dinner.data},
-                       'viernes': {'lunch': {'first': form.friday_lunch_starter.data, 'second': form.friday_lunch_main.data}, 'dinner': form.friday_dinner.data}}
-        flash(weekly_plan)
+        weekly_plan = {}
+        if form.saturday_lunch_starter.data and form.saturday_lunch_starter.data != STARTERS[0][0]:
+            weekly_plan.setdefault("sabado", {}).setdefault('lunch', {})['first'] =  form.saturday_lunch_starter.data
+        if form.saturday_lunch_main.data and form.saturday_lunch_main.data != MAIN_COURSES[0][0]:
+            weekly_plan.setdefault("sabado", {}).setdefault('lunch', {})['second'] = form.saturday_lunch_main.data
+        if form.saturday_dinner.data and form.saturday_dinner.data != DINNER[0][0]:
+            weekly_plan.setdefault("sabado", {})['dinner'] = form.saturday_dinner.data
+        if form.sunday_lunch_starter.data and form.sunday_lunch_starter.data != STARTERS[0][0]:
+            weekly_plan.setdefault("domingo", {}).setdefault('lunch', {})['first'] =  form.sunday_lunch_starter.data
+        if form.sunday_lunch_main.data and form.sunday_lunch_main.data != MAIN_COURSES[0][0]:
+            weekly_plan.setdefault("domingo", {}).setdefault('lunch', {})['second'] = form.sunday_lunch_main.data
+        if form.sunday_dinner.data and form.sunday_dinner.data != DINNER[0][0]:
+            weekly_plan.setdefault("domingo", {})['dinner'] = form.sunday_dinner.data
+        if form.monday_lunch_starter.data and form.monday_lunch_starter.data != STARTERS[0][0]:
+            weekly_plan.setdefault("lunes", {}).setdefault('lunch', {})['first'] =  form.monday_lunch_starter.data
+        if form.monday_lunch_main.data and form.monday_lunch_main.data != MAIN_COURSES[0][0]:
+            weekly_plan.setdefault("lunes", {}).setdefault('lunch', {})['second'] = form.monday_lunch_main.data
+        if form.monday_dinner.data and form.monday_dinner.data != DINNER[0][0]:
+            weekly_plan.setdefault("lunes", {})['dinner'] = form.monday_dinner.data
+        if form.tuesday_lunch_starter.data and form.tuesday_lunch_starter.data != STARTERS[0][0]:
+            weekly_plan.setdefault("martes", {}).setdefault('lunch', {})['first'] =  form.tuesday_lunch_starter.data
+        if form.tuesday_lunch_main.data and form.tuesday_lunch_main.data != MAIN_COURSES[0][0]:
+            weekly_plan.setdefault("martes", {}).setdefault('lunch', {})['second'] = form.tuesday_lunch_main.data
+        if form.tuesday_dinner.data and form.tuesday_dinner.data != DINNER[0][0]:
+            weekly_plan.setdefault("martes", {})['dinner'] = form.tuesday_dinner.data
+        if form.wednesday_lunch_starter.data and form.wednesday_lunch_starter.data != STARTERS[0][0]:
+            weekly_plan.setdefault("miercoles", {}).setdefault('lunch', {})['first'] =  form.wednesday_lunch_starter.data
+        if form.wednesday_lunch_main.data and form.wednesday_lunch_main.data != MAIN_COURSES[0][0]:
+            weekly_plan.setdefault("miercoles", {}).setdefault('lunch', {})['second'] = form.wednesday_lunch_main.data
+        if form.wednesday_dinner.data and form.wednesday_dinner.data != DINNER[0][0]:
+            weekly_plan.setdefault("miercoles", {})['dinner'] = form.wednesday_dinner.data
+        if form.thursday_lunch_starter.data and form.thursday_lunch_starter.data != STARTERS[0][0]:
+            weekly_plan.setdefault("jueves", {}).setdefault('lunch', {})['first'] =  form.thursday_lunch_starter.data
+        if form.thursday_lunch_main.data and form.thursday_lunch_main.data != MAIN_COURSES[0][0]:
+            weekly_plan.setdefault("jueves", {}).setdefault('lunch', {})['second'] = form.thursday_lunch_main.data
+        if form.thursday_dinner.data and form.thursday_dinner.data != DINNER[0][0]:
+            weekly_plan.setdefault("jueves", {})['dinner'] = form.thursday_dinner.data
+        if form.friday_lunch_starter.data and form.friday_lunch_starter.data != STARTERS[0][0]:
+            weekly_plan.setdefault("viernes", {}).setdefault('lunch', {})['first'] =  form.friday_lunch_starter.data
+        if form.friday_lunch_main.data and form.friday_lunch_main.data != MAIN_COURSES[0][0]:
+            weekly_plan.setdefault("viernes", {}).setdefault('lunch', {})['second'] = form.friday_lunch_main.data
+        if form.friday_dinner.data and form.friday_dinner.data != DINNER[0][0]:
+            weekly_plan.setdefault("viernes", {})['dinner'] = form.friday_dinner.data
+
+        flash(build_whatsapp_meal_plan(weekly_plan))
     return render_template('planner.html', form=form)
