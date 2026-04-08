ff10c58c9b23f5c8823b20496f166d3ff60c4efa
Alberto M <traquinedes@yahoo.es>
2026-04-08 11:38:57 +0200

    feat[web-ui]: load meals from meals json file

--- src/webui/app.py
+++ src/webui/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, flash
-from webui.forms import WeeklyPlannerForm, STARTERS, MAIN_COURSES, DINNER
+from webui.forms import WeeklyPlannerForm
 from message_templates.whatsapp import build_whatsapp_meal_plan
+from mealsstore.jsonloader import JsonLoader
 
 import os
 SECRET_KEY = os.urandom(32)
@@ -11,50 +12,56 @@ app.config['SECRET_KEY'] = SECRET_KEY
 
 @app.route('/weekly-meals-planner', methods=['GET', 'POST'])
 def index():
+    loader = JsonLoader("meals.json")
+    starters = loader.starters()
+    main_courses = loader.main_courses()
+    dinners = loader.dinners()
+
     form = WeeklyPlannerForm()
+    
     if form.validate_on_submit():
         weekly_plan = {}
-        if form.saturday_lunch_starter.data and form.saturday_lunch_starter.data != STARTERS[0][0]:
+        if form.saturday_lunch_starter.data and form.saturday_lunch_starter.data != starters[0][0]:
             weekly_plan.setdefault("sabado", {}).setdefault('lunch', {})['first'] =  form.saturday_lunch_starter.data
-        if form.saturday_lunch_main.data and form.saturday_lunch_main.data != MAIN_COURSES[0][0]:
+        if form.saturday_lunch_main.data and form.saturday_lunch_main.data != main_courses[0][0]:
             weekly_plan.setdefault("sabado", {}).setdefault('lunch', {})['second'] = form.saturday_lunch_main.data
-        if form.saturday_dinner.data and form.saturday_dinner.data != DINNER[0][0]:
+        if form.saturday_dinner.data and form.saturday_dinner.data != dinners[0][0]:
             weekly_plan.setdefault("sabado", {})['dinner'] = form.saturday_dinner.data
-        if form.sunday_lunch_starter.data and form.sunday_lunch_starter.data != STARTERS[0][0]:
+        if form.sunday_lunch_starter.data and form.sunday_lunch_starter.data != starters[0][0]:
             weekly_plan.setdefault("domingo", {}).setdefault('lunch', {})['first'] =  form.sunday_lunch_starter.data
-        if form.sunday_lunch_main.data and form.sunday_lunch_main.data != MAIN_COURSES[0][0]:
+        if form.sunday_lunch_main.data and form.sunday_lunch_main.data != main_courses[0][0]:
             weekly_plan.setdefault("domingo", {}).setdefault('lunch', {})['second'] = form.sunday_lunch_main.data
-        if form.sunday_dinner.data and form.sunday_dinner.data != DINNER[0][0]:
+        if form.sunday_dinner.data and form.sunday_dinner.data != dinners[0][0]:
             weekly_plan.setdefault("domingo", {})['dinner'] = form.sunday_dinner.data
-        if form.monday_lunch_starter.data and form.monday_lunch_starter.data != STARTERS[0][0]:
+        if form.monday_lunch_starter.data and form.monday_lunch_starter.data != starters[0][0]:
             weekly_plan.setdefault("lunes", {}).setdefault('lunch', {})['first'] =  form.monday_lunch_starter.data
-        if form.monday_lunch_main.data and form.monday_lunch_main.data != MAIN_COURSES[0][0]:
+        if form.monday_lunch_main.data and form.monday_lunch_main.data != main_courses[0][0]:
             weekly_plan.setdefault("lunes", {}).setdefault('lunch', {})['second'] = form.monday_lunch_main.data
-        if form.monday_dinner.data and form.monday_dinner.data != DINNER[0][0]:
+        if form.monday_dinner.data and form.monday_dinner.data != dinners[0][0]:
             weekly_plan.setdefault("lunes", {})['dinner'] = form.monday_dinner.data
-        if form.tuesday_lunch_starter.data and form.tuesday_lunch_starter.data != STARTERS[0][0]:
+        if form.tuesday_lunch_starter.data and form.tuesday_lunch_starter.data != starters[0][0]:
             weekly_plan.setdefault("martes", {}).setdefault('lunch', {})['first'] =  form.tuesday_lunch_starter.data
-        if form.tuesday_lunch_main.data and form.tuesday_lunch_main.data != MAIN_COURSES[0][0]:
+        if form.tuesday_lunch_main.data and form.tuesday_lunch_main.data != main_courses[0][0]:
             weekly_plan.setdefault("martes", {}).setdefault('lunch', {})['second'] = form.tuesday_lunch_main.data
-        if form.tuesday_dinner.data and form.tuesday_dinner.data != DINNER[0][0]:
+        if form.tuesday_dinner.data and form.tuesday_dinner.data != dinners[0][0]:
             weekly_plan.setdefault("martes", {})['dinner'] = form.tuesday_dinner.data
-        if form.wednesday_lunch_starter.data and form.wednesday_lunch_starter.data != STARTERS[0][0]:
+        if form.wednesday_lunch_starter.data and form.wednesday_lunch_starter.data != starters[0][0]:
             weekly_plan.setdefault("miercoles", {}).setdefault('lunch', {})['first'] =  form.wednesday_lunch_starter.data
-        if form.wednesday_lunch_main.data and form.wednesday_lunch_main.data != MAIN_COURSES[0][0]:
+        if form.wednesday_lunch_main.data and form.wednesday_lunch_main.data != main_courses[0][0]:
             weekly_plan.setdefault("miercoles", {}).setdefault('lunch', {})['second'] = form.wednesday_lunch_main.data
-        if form.wednesday_dinner.data and form.wednesday_dinner.data != DINNER[0][0]:
+        if form.wednesday_dinner.data and form.wednesday_dinner.data != dinners[0][0]:
             weekly_plan.setdefault("miercoles", {})['dinner'] = form.wednesday_dinner.data
-        if form.thursday_lunch_starter.data and form.thursday_lunch_starter.data != STARTERS[0][0]:
+        if form.thursday_lunch_starter.data and form.thursday_lunch_starter.data != starters[0][0]:
             weekly_plan.setdefault("jueves", {}).setdefault('lunch', {})['first'] =  form.thursday_lunch_starter.data
-        if form.thursday_lunch_main.data and form.thursday_lunch_main.data != MAIN_COURSES[0][0]:
+        if form.thursday_lunch_main.data and form.thursday_lunch_main.data != main_courses[0][0]:
             weekly_plan.setdefault("jueves", {}).setdefault('lunch', {})['second'] = form.thursday_lunch_main.data
-        if form.thursday_dinner.data and form.thursday_dinner.data != DINNER[0][0]:
+        if form.thursday_dinner.data and form.thursday_dinner.data != dinners[0][0]:
             weekly_plan.setdefault("jueves", {})['dinner'] = form.thursday_dinner.data
-        if form.friday_lunch_starter.data and form.friday_lunch_starter.data != STARTERS[0][0]:
+        if form.friday_lunch_starter.data and form.friday_lunch_starter.data != starters[0][0]:
             weekly_plan.setdefault("viernes", {}).setdefault('lunch', {})['first'] =  form.friday_lunch_starter.data
-        if form.friday_lunch_main.data and form.friday_lunch_main.data != MAIN_COURSES[0][0]:
+        if form.friday_lunch_main.data and form.friday_lunch_main.data != main_courses[0][0]:
             weekly_plan.setdefault("viernes", {}).setdefault('lunch', {})['second'] = form.friday_lunch_main.data
-        if form.friday_dinner.data and form.friday_dinner.data != DINNER[0][0]:
+        if form.friday_dinner.data and form.friday_dinner.data != dinners[0][0]:
             weekly_plan.setdefault("viernes", {})['dinner'] = form.friday_dinner.data
 
         flash(build_whatsapp_meal_plan(weekly_plan))
--- src/webui/forms.py
+++ src/webui/forms.py
@@ -1,68 +1,33 @@
 from flask_wtf import FlaskForm
 from wtforms import SelectField, SubmitField
+from mealsstore.jsonloader import JsonLoader
 
-STARTERS = [('---', '---'), 
-            ('Alubias con Calamar', 'Alubias con Calamar'), 
-            ('Garbanzos con Bakalao', 'Garbanzos con Bakalao'), 
-            ('Garbanzos con Langostinos', 'Garbanzos con Langostinos'),
-            ('Lentejas con Rape', 'Lentejas con Rape'), 
-            ('Judias Verdes', 'Judias Verdes'),
-            ('Crema de Calabacín', 'Crema de Calabacín'),
-            ('Ensaladilla', 'Ensaladilla'),
-            ('Pisto con Huevo', 'Pisto con Huevo'),
-            ('Paella','Paella'),
-            ('Macarrones con Carne','Macarrones con Carne'),
-            ('Pasta Carbonara','Pasta Carbonara'),
-            ('Lasaña de Espinacas','Lasaña de Espinacas'),
-            ('Sopa Cocido','Sopa Cocido'),]
-
-MAIN_COURSES = [('---', '---'), 
-                ('Filetes de Maza', 'Filetes de Maza'),
-                ('Filetes de Cabecero', 'Filetes de Cabecero'),
-                ('Muslos de Pollo Plancha', 'Muslos de Pollo Plancha'),
-                ('Muslos Pollo Asados', 'Muslos Pollo Asados'),
-                ('Hamburguesa Completa', 'Hamburguesa Completa'),
-                ('Hamburguesa', 'Hamburguesa'),
-                ('Escalopines', 'Escalopines'),
-                ('Chuleta Cerdo', 'Chuleta Cerdo'),
-                ('Solomillo Cerdo', 'Solomillo Cerdo'),
-                ('Carne a la Jardinera', 'Carne a la Jardinera'),
-                ('Cocido', 'Cocido'),
-                ('Bakalao Vizcaina', 'Bakalao Vizcaina'),
-                ('Calamares en su Tinta', 'Calamares en su Tinta'),]
-
-DINNER = [('---', '---'), 
-          ('Sopa', 'Sopa'), 
-          ('Tortilla Francesa', 'Tortilla Francesa'), 
-          ('Tortilla Patata', 'Tortilla Patata'),
-          ('Huevos Duros', 'Huevos Duros'),
-          ('Bakalao Vizcaina', 'Bakalao Vizcaina'), 
-          ('Pescado Rebozado', 'Pescado Rebozado'), 
-          ('Sushi', 'Sushi'), 
-          ('Calamares en su Tinta', 'Calamares en su Tinta'), 
-          ('Filetes Lomo', 'Filetes Lomo'), 
-          ('Pechuga Empanada', 'Pechuga Empanada')]
 
 class WeeklyPlannerForm(FlaskForm):
-    saturday_lunch_starter = SelectField('Saturday Lunch Starter', choices=STARTERS)
-    saturday_lunch_main = SelectField('Saturday Lunch Starter', choices=MAIN_COURSES)
-    saturday_dinner = SelectField('Saturday Lunch Starter', choices=DINNER)
-    sunday_lunch_starter = SelectField('Sunday Lunch Starter', choices=STARTERS)
-    sunday_lunch_main = SelectField('Sunday Lunch Starter', choices=MAIN_COURSES)
-    sunday_dinner = SelectField('Sunday Lunch Starter', choices=DINNER)
-    monday_lunch_starter = SelectField('Monday Lunch Starter', choices=STARTERS)
-    monday_lunch_main = SelectField('Monday Lunch Starter', choices=MAIN_COURSES)
-    monday_dinner = SelectField('Monday Lunch Starter', choices=DINNER)
-    tuesday_lunch_starter = SelectField('Tuesday Lunch Starter', choices=STARTERS)
-    tuesday_lunch_main = SelectField('Tuesday Lunch Starter', choices=MAIN_COURSES)
-    tuesday_dinner = SelectField('Tuesday Lunch Starter', choices=DINNER)
-    wednesday_lunch_starter = SelectField('Wednesday Lunch Starter', choices=STARTERS)
-    wednesday_lunch_main = SelectField('Wednesday Lunch Starter', choices=MAIN_COURSES)
-    wednesday_dinner = SelectField('Wednesday Lunch Starter', choices=DINNER)
-    thursday_lunch_starter = SelectField('Thursday Lunch Starter', choices=STARTERS)
-    thursday_lunch_main = SelectField('Thursday Lunch Starter', choices=MAIN_COURSES)
-    thursday_dinner = SelectField('Thursday Lunch Starter', choices=DINNER)
-    friday_lunch_starter = SelectField('Friday Lunch Starter', choices=STARTERS)
-    friday_lunch_main = SelectField('Friday Lunch Starter', choices=MAIN_COURSES)
-    friday_dinner = SelectField('Friday Lunch Starter', choices=DINNER)
+    loader = JsonLoader("meals.json")
+    starters = loader.starters()
+    main_courses = loader.main_courses()
+    dinners = loader.dinners()
+
+    saturday_lunch_starter = SelectField('Saturday Lunch Starter', choices=starters)
+    saturday_lunch_main = SelectField('Saturday Lunch Starter', choices=main_courses)
+    saturday_dinner = SelectField('Saturday Lunch Starter', choices=dinners)
+    sunday_lunch_starter = SelectField('Sunday Lunch Starter', choices=starters)
+    sunday_lunch_main = SelectField('Sunday Lunch Starter', choices=main_courses)
+    sunday_dinner = SelectField('Sunday Lunch Starter', choices=dinners)
+    monday_lunch_starter = SelectField('Monday Lunch Starter', choices=starters)
+    monday_lunch_main = SelectField('Monday Lunch Starter', choices=main_courses)
+    monday_dinner = SelectField('Monday Lunch Starter', choices=dinners)
+    tuesday_lunch_starter = SelectField('Tuesday Lunch Starter', choices=starters)
+    tuesday_lunch_main = SelectField('Tuesday Lunch Starter', choices=main_courses)
+    tuesday_dinner = SelectField('Tuesday Lunch Starter', choices=dinners)
+    wednesday_lunch_starter = SelectField('Wednesday Lunch Starter', choices=starters)
+    wednesday_lunch_main = SelectField('Wednesday Lunch Starter', choices=main_courses)
+    wednesday_dinner = SelectField('Wednesday Lunch Starter', choices=dinners)
+    thursday_lunch_starter = SelectField('Thursday Lunch Starter', choices=starters)
+    thursday_lunch_main = SelectField('Thursday Lunch Starter', choices=main_courses)
+    thursday_dinner = SelectField('Thursday Lunch Starter', choices=dinners)
+    friday_lunch_starter = SelectField('Friday Lunch Starter', choices=starters)
+    friday_lunch_main = SelectField('Friday Lunch Starter', choices=main_courses)
+    friday_dinner = SelectField('Friday Lunch Starter', choices=dinners)
     submit = SubmitField('Generar Plan')
