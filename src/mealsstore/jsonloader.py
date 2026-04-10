6e22c3069000ce0647e2989c84f10ac7ca472e29
Alberto M <traquinedes@yahoo.es>
2026-04-10 10:37:39 +0200

    fix[meals-store]: If file does not exists try default path

--- src/mealsstore/jsonloader.py
+++ src/mealsstore/jsonloader.py
@@ -4,10 +4,15 @@ from mealsstore.mealsloader import MealsLoader
 import json
 
 class JsonLoader(MealsLoader):
+    default_json_file_path = "meals.json"
 
     def __init__(self, path: str) -> None:
-        with open(path, 'r', encoding='utf-8') as file:
-            self.data = json.load(file)
+        try:
+            with open(path, 'r', encoding='utf-8') as file:
+                self.data = json.load(file)
+        except FileNotFoundError:
+            with open(self.default_json_file_path, 'r', encoding='utf-8') as file:
+                self.data = json.load(file)
     
     def starters(self) -> List[Tuple[str, str]]:
         return self._meals("starters")
